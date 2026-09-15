391c7d18a23e499119f21078b74c65a07f473bee
RiverDave <davidriverg@gmail.com>
2026-09-14 22:42:28 -0400

    [CIR] dyn_cast_or_null for the guarded terminator rewrites

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -5108,8 +5108,9 @@ struct ConvertCIROffloadToGPUPass
 
         // Convert cir.return terminators to gpu.return inside the new gpu.func.
         for (mlir::Block &blk : gpuFn.getBody()) {
-          if (auto ret = dyn_cast<cir::ReturnOp>(blk.mightHaveTerminator() ? blk.getTerminator()
-                                                       : nullptr)) {
+          if (auto ret = dyn_cast_or_null<cir::ReturnOp>(blk.mightHaveTerminator()
+                                    ? blk.getTerminator()
+                                    : nullptr)) {
             mlir::OpBuilder::InsertionGuard rg(builder);
             builder.setInsertionPoint(ret);
             mlir::gpu::ReturnOp::create(builder, ret.getLoc(),
@@ -6012,8 +6013,9 @@ struct ConvertCIRInGpuModulePass
     // the main CIR→LLVM conversion (which would turn them into llvm.return).
     gpuModule.walk([&](mlir::gpu::GPUFuncOp fn) {
       for (mlir::Block &blk : fn.getBody()) {
-        if (auto ret = dyn_cast<cir::ReturnOp>(blk.mightHaveTerminator() ? blk.getTerminator()
-                                                       : nullptr)) {
+        if (auto ret = dyn_cast_or_null<cir::ReturnOp>(blk.mightHaveTerminator()
+                                    ? blk.getTerminator()
+                                    : nullptr)) {
           mlir::OpBuilder b(ret);
           mlir::gpu::ReturnOp::create(b, ret.getLoc(), ret.getOperands());
           ret.erase();
